7855f1035a4554562a4e7591b55cf28fa38cb8a8
Anubhab <mukerji29anuvb@gmail.com>
2021-07-05 10:18:41 +0530

    Initial Commit: making the App work again

---
+++ src/pages/ViewContact.js
@@ -0,0 +1,71 @@
+import React, { useContext } from "react";
+
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardImg,
+} from "reactstrap";
+import { FaEnvelope, FaMapMarkerAlt, FaPhone } from "react-icons/fa";
+import { ContactContext } from "../context/Context";
+import { Link } from "react-router-dom";
+
+const ViewContact = () => {
+  const { state } = useContext(ContactContext);
+  // destructuring contact from the state
+  // and rendering it in state
+
+  const { contact } = state;
+
+  return (
+    <Container>
+      <Row className="mt-5 mb-5">
+        <Col md="5" className="offset-md-3">
+          <Card className="pt-3 pb-5">
+            <CardBody className="text-center ">
+              <CardImg
+                height="150"
+                width="150"
+                className="cardImg profile border-danger"
+                src={contact?.picture}
+              />
+              <CardTitle className="text-primary mt-3">
+                <h1>{contact?.name}</h1>
+              </CardTitle>
+              <CardSubtitle>
+                <h3>
+                  <FaPhone className="mr-2" />
+                  {contact?.phoneNumber}
+                </h3>
+              </CardSubtitle>
+              <Link
+                className="btn btn-primary btn-block"
+                target="_blank"
+                href={`mailto:{contact?.email}`}
+              >
+                <FaEnvelope className="icon mr-2" />
+                {contact?.email}
+              </Link>
+
+              <Link
+                className="btn btn-primary btn-block"
+                target="_blank"
+                //TODO: add google maps
+                href={`https://maps.google.com/?=${contact?.address}`}
+              >
+                <FaMapMarkerAlt className="icon mr-2" />
+                {contact?.address}
+              </Link>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ViewContact;
---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+  //TODO: ADD YOUR FIREBASE INFO HERE - DONE:
+  apiKey: "AIzaSyDpSYhW2hI8qEZHFXmdqZDmI-dxNi-M8yo",
+  authDomain: "reactlco.firebaseapp.com",
+  databaseURL: "https://reactlco-default-rtdb.firebaseio.com",
+  projectId: "reactlco",
+  storageBucket: "reactlco.appspot.com",
+  messagingSenderId: "87984293007",
+  appId: "1:87984293007:web:efe3dd724c8abdff9c5cfa",
+  measurementId: "G-MPPNTK91HM",
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
