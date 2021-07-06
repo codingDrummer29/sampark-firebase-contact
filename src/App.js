247d1893678bfba3247f3c39d6cbae4f5905ca2b
Anubhab <mukerji29anuvb@gmail.com>
2021-07-06 17:07:24 +0530

    getting all data from firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useReducer, useEffect } from "react";
 
-import { Container, Col, Row } from "reactstrap";
+import { Container } from "reactstrap";
 
 // react-router-dom3
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // react toastify stuffs
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 // bootstrap css
@@ -14,7 +14,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 // firebase stuffs
-//TODO: import firebase config and firebase database
+//TODO: import firebase config and firebase database - DONE:
+import { firebaseConfig } from "./utils/config";
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/storage";
 
 // components
 import AddContact from "./pages/AddContact";
@@ -25,10 +29,14 @@ import ViewContact from "./pages/ViewContact";
 import PageNotFound from "./pages/PageNotFound";
 
 // context api stuffs
-//TODO: import reducers and contexts
+//TODO: import reducers and contexts - DONE:
+import { ContactContext } from "./context/Context";
+import reducer from "./context/reducer";
+import { SET_CONTACT, SET_LOADING } from "./context/action.types";
 
 //initlizeing firebase app with the firebase config which are in ./utils/firebaseConfig
-//TODO: initialize FIREBASE
+//TODO: initialize FIREBASE - DONE:
+firebase.initializeApp(firebaseConfig);
 
 // first state to provide in react reducer
 const initialState = {
@@ -36,7 +44,7 @@ const initialState = {
   contact: {},
   contactToUpdate: null,
   contactToUpdateKey: null,
-  isLoading: false
+  isLoading: false,
 };
 
 const App = () => {
@@ -44,18 +52,35 @@ const App = () => {
 
   // will get contacts from firebase and set it on state contacts array
   const getContacts = async () => {
-    // TODO: load existing data
+    // TODO: load existing data - DONE:
+    dispatch({
+      type: SET_LOADING,
+      payload: true,
+    });
+
+    const contactsRef = await firebase.database().ref("/contacts");
+    contactsRef.on("value", (snapshot) => {
+      dispatch({
+        type: SET_CONTACT,
+        payload: snapshot.val(),
+      });
+      dispatch({
+        type: SET_LOADING,
+        payload: false,
+      });
+    });
   };
 
   // getting contact  when component did mount
   useEffect(() => {
-    //FIXME: call methods if needed
+    //FIXME: call methods if needed - DONE:
+    getContacts();
   }, []);
 
   return (
     <Router>
-      {/* FIXME: Provider is not configured */}
-      <ContactContext.Provider>
+      {/* FIXME: Provider is not configured - DONE:*/}
+      <ContactContext.Provider value={{ state, dispatch }}>
         <ToastContainer />
         <Header />
         <Container>
