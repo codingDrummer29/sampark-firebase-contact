7be6a11bdc484a20f8c645e089e66b498af77f4e
Anubhab <mukerji29anuvb@gmail.com>
2021-07-06 13:05:46 +0530

    handling context done

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,1 +1,4 @@
-//TODO: Create context: ContactContext
+//TODO: Create context: ContactContext - DONE:
+import { createContext } from "react";
+
+export const ContactContext = createContext();
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,10 +1,41 @@
-//TODO: create contact using all actions
+/* eslint-disable no-unreachable */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+//TODO: create contact using all actions - DONE:
 
 import {
   SET_CONTACT,
   SET_LOADING,
   CONTACT_TO_UPDATE,
-  SET_SINGLE_CONTACT
+  SET_SINGLE_CONTACT,
 } from "./action.types";
 
-//TODO: use switch case
+//TODO: use switch case - DONE:
+export default () => {
+  switch (action.type) {
+    case SET_CONTACT:
+      return action.payload == null
+        ? { ...state, contacts: [] }
+        : { ...state, contacts: action.payload };
+
+    case SET_LOADING:
+      return { ...state, isLoading: action.payload };
+
+    case CONTACT_TO_UPDATE:
+      return {
+        ...state,
+        contactToUpdate: action.payload,
+        contactToUpdateKey: action.key,
+      };
+
+    case SET_SINGLE_CONTACT:
+      return {
+        ...state,
+        contact: action,
+        payload,
+      };
+
+    default:
+      return state;
+  }
+};
